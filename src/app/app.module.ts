86175565b55cccc361039e66dde9bb72de0abc8d
Mark Mizen <mark.c.mizen@gmail.com>
2019-09-12 23:20:19 +0100

    added home path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderModule } from './app-header/app-header.module';
 import { SecuritiesDashboardModule } from './securities-dashboard/securities-dashboard.module';
+import { HomeComponent } from './home/home.component';
 
 
 
  @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
